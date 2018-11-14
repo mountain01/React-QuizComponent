513a057311129d642df9d55d92d7ea02edeab50d
Matt Edwards <matt.edwards@domo.com>
2018-11-13 17:29:05 -0700

    reset

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,10 +15,13 @@ class Quiz extends Component {
     const isQuizEnd = this.state.quiz_position-1 === quizData.quiz_questions.length;
     return (
       <div>
-        {isQuizEnd? <QuizEnd />:
+        {isQuizEnd? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/>:
         <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>}
       </div>
     )
+  }
+  handleResetClick(){
+    this.setState(state=>({quiz_position:1}));
   }
   showNextQuestion(){
     this.setState(state=>({quiz_position:state.quiz_position+1}))
