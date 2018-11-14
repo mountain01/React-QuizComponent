04051cff3b99e145e33ea3a3db987a75ed9da690
Matt Edwards <matt.edwards@domo.com>
2018-11-13 17:25:16 -0700

    display error

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       incorrectAnswer:false
+    }
+  }
+  
   render() {
     return (
       <div>
@@ -17,6 +25,7 @@ class QuizQuestion extends Component {
         })}
           </ul>
         </section>
+        {this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p>: null}
       </main>
       </div>
     )
@@ -24,6 +33,9 @@ class QuizQuestion extends Component {
   handleClick(buttonText){
     if(buttonText===this.props.quiz_question.answer){
       this.props.showNextQuestionHandler();
+      this.setState(state=>({incorrectAnswer:false}));
+    } else {
+      this.setState(state=>({incorrectAnswer:true}));
     }
   }
 }
